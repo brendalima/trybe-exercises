ee4c01a84be9b8c854206d06302ba0308a92662a
brendalima <brenda.c.lima@poli.ufrj.br>
2020-08-27 18:10:06 -0300

    exercicio inicial

---
+++ bloco_4/4_1/adicao.js
@@ -0,0 +1,22 @@
+let a = 1;
+let b = 2;
+
+// soma
+let soma = a + b;
+console.log(soma);
+
+// subtração
+let subtracao = a - b;
+console.log(subtracao);
+
+// multiplicação
+let multiplicacao = a * b;
+console.log(multiplicacao);
+
+//divisão
+let divisao = a / b;
+console.log(divisao);
+
+//módulo
+let modulo = a % b;
+console.log(modulo);
