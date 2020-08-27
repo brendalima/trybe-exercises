b1e3feadbdf12e486eaf8c611a71e9663cb638ef
brendalima <brenda.c.lima@poli.ufrj.br>
2020-08-27 18:18:26 -0300

    adiciona exercicio 3 maior de tres numeros

---
+++ bloco_4/4_1/maior-de-tres.js
@@ -0,0 +1,13 @@
+let num1 = 3;
+let num2 = 6;
+let num3 = 1;
+
+if (num1 > num2 && num1 > num3) {
+    console.log(num1);
+}
+else if (num2 > num1 && num2 > num3) {
+    console.log(num2);
+}
+else if (num3 > num1 && num3 > num2) {
+    console.log(num3);
+}
