b66752017ff05d9c9e6ee0545e80161d46f011c0
brendalima <brenda.c.lima@poli.ufrj.br>
2020-09-17 19:46:00 -0300

    adding first field of form, with name, email, cpf, address, city, estate and type of housing

---
+++ bloco_6/dia-1/script.js
@@ -0,0 +1,36 @@
+const estatesListBR = [
+    'Acre',
+    'Alagoas',
+    'Amapá',
+    'Amazonas',
+    'Bahia',
+    'Ceará',
+    'Distrito Federal',
+    'Espírito Santo',
+    'Goiás',
+    'Maranhão',
+    'Mato Grosso',
+    'Mato Grosso do Sul',
+    'Minas Gerais',
+    'Pará',
+    'Paraíba',
+    'Paraná',
+    'Pernambuco',
+    'Piauí',
+    'Rio de Janeiro',
+    'Rio Grande do Norte',
+    'Rio Grande do Sul',
+    'Rondônia',
+    'Roraima',
+    'Santa Catarina',
+    'São Paulo',
+    'Sergipe',
+    'Tocantins'
+];
+
+for (let index = 0; index < estatesListBR.length; index += 1) {
+    let list = document.getElementById('estate');
+    let item = document.createElement('option');
+    item.innerHTML = estatesListBR[index];
+    list.appendChild(item);
+}
